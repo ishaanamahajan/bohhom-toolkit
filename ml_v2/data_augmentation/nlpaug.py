49958e4cfca908150ee086e8fc8753af803333fe
qiwu-zhang <qiwu.zhang@mail.utoronto.ca>
2022-06-22 03:24:54 -0400

    update

--- ml_v2/data_augmentation/nlpaug.py
+++
@@ -1,7 +0,0 @@
-import nlpaug.augmenter.word as naw
-
-syn_aug = naw.SynonymAug(aug_p=0.3)
-sentence = 'climate change puts the squeeze on wine production'
-mod_sentence = syn_aug.augment(sentence, n=1)
-print('Original text :', sentence)
-print('Augmented text :', mod_sentence)
